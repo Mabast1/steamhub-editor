f78caa9bc40ada897724b7529338dd97bee03da8
Rasuna Khatami <imuhammad@na.edu>
2019-07-16 10:58:46 -0500

    Initialize v2.0

--- src/components/tabs/uploadBtn/uploadBtn-styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(theme => ({
-  browseBtnRoot: {
-    width: '120px'
-  }
-}));
-
-export default useStyles;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app';
-import Firebase, { FirebaseContext } from './components/firebase';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: '"Nunito Sans", -apple-system, BlinkMacSystemFont, sans-serif'
-  }
-});
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </FirebaseContext.Provider>,
-  document.getElementById('root')
-);
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
